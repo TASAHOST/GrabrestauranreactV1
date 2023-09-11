d803ffd92c888c6bcc4f4dac6ef2348ab2e8c317
TASAHOST <120707750+TASAHOST@users.noreply.github.com>
2023-09-11 10:55:10 +0700

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import NavBar from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Restaurant from './pages/Restaurant'
+import Add from './pages/Add'
+import Search from './pages/Search'
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+    <NavBar/>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Restaurant />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/Search" element={<Search />} />
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Restaurant.jsx
@@ -0,0 +1,33 @@
+import {useState, useEffect} from 'react';
+import {Link} from 'react-router-dom';
+import axios from "axios";
+
+const URL = import.meta.env.VITE_BASE_URL;
+const USERNAME = import.meta.env.VITE_BASE_USERNAME;
+const PASSWORD = import.meta.env.VITE_BASE_PASSWORD;
+const config = {
+    auth: {
+        username:USERNAME,
+        password:PASSWORD,
+    },
+};
+
+const Restaurant = () => {
+    const [restaurants, setRestaurants] = useState([]);
+    useEffect(()=>{
+        const fetcAllRestaurant = async () =>{
+            try {
+                const res = await axios.get(`${URL}/restaurants`, config)
+                setRestaurants(res.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetcAllRestaurant();
+    },[])
+  return (
+    <div>Restaurant</div>
+  )
+}
+
+export default Restaurant
